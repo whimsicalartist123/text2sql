627337ef13796b3a41aaff3cc79fb835a02c4fe0
whimsicalartist123 <nawalgaria.utkarsh8@gmail.com>
2020-03-30 08:34:36 +0530

    minor change

---
+++ src/models/sqlnet.py
@@ -0,0 +1,18 @@
+import os
+import json
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+
+class SQLNet(nn.Module):
+
+    def __init__(self):
+        super(SQLNet, self).__init__()
+        pass
+    
+    def forward(self):
+        pass
+    
